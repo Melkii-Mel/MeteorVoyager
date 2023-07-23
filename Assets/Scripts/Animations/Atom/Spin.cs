e1a634274920664b5f77c04b720966c631c364a6
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2023-07-23 19:45:15 +0500

    Matter visualization added
    yeeee

---
+++ Assets/Scripts/Animations/Atom/Spin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Animations.Atom
+{
+    public class Spin : MonoBehaviour
+    {
+        [SerializeField] private float rotationX;
+        [SerializeField] private float rotationY;
+        [SerializeField] private float rotationZ;
+        [SerializeField] private float coeff;
+
+        private void Update()
+        {
+            transform.Rotate(rotationX * coeff, rotationY * coeff, rotationZ * coeff);
+        }
+    }
+}
