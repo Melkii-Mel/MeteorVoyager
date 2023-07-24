53ab97e5834381b178dd79597c83b6c4b8977879
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2023-07-24 20:11:54 +0500

    adding component to a prefab fix
    that was stupid

--- Assets/Scripts/MonoBehaviours/ScreenExplosion.cs
+++ Assets/Scripts/MonoBehaviours/ScreenExplosion.cs
@@ -8,6 +8,50 @@ namespace MonoBehaviours
 {
     public class ScreenExplosion : MonoBehaviour
     {
+        [SerializeField] private GameObject objPrefab;
+        private GameObject _objPrefabCopy;
+        private const float MS_PER_TICK = 1000;
+        private float _msBuffer;
+        private readonly Random _random = new();
+        private readonly Func<int, float> _chanceFormula = (x) => 1 / (Mathf.Log((x / 500f + 3), 3));
+
+        private void OnEnable()
+        {
+            GlobalTimer.OnTick += ControlExplosion;
+        }
+
+        private void OnDisable()
+        {
+            GlobalTimer.OnTick -= ControlExplosion;
+        }
+
+        private void Start()
+        {
+            _objPrefabCopy = Instantiate(objPrefab);
+            _objPrefabCopy.SetActive(false);
+            _objPrefabCopy.AddComponent<ExplosionObj>();
+        }
+
+        private void ControlExplosion(float deltaTimeMS)
+        {
+            _msBuffer += deltaTimeMS;
+            while (_msBuffer > MS_PER_TICK)
+            {
+                _msBuffer -= MS_PER_TICK;
+                float v1 = (float)_random.NextDouble();
+                float v2 = _chanceFormula(GameStats.MainGameStatsHolder.DataUpgrades.ScreenExplosionLvl);
+                if (v1 > v2)
+                {
+                    Explode();
+                }
+            }
+        }
+        
+        private void Explode()
+        {
+            Instantiate(_objPrefabCopy, gameObject.transform, false).SetActive(true);
+        }
+        
         [Serializable]
         private class ExplosionObj : MonoBehaviour
         {
@@ -36,47 +80,6 @@ namespace MonoBehaviours
                     enemy.TakeDamage(_damage);
                 }
             }
-        }
-
-        [SerializeField] private GameObject objPrefab;
-        private const float MS_PER_TICK = 1000;
-        private float _msBuffer;
-        private readonly Random _random = new();
-        private readonly Func<int, float> _chanceFormula = (x) => 1 / (Mathf.Log((x / 500f + 3), 3));
-
-        private void OnEnable()
-        {
-            GlobalTimer.OnTick += ControlExplosion;
-        }
-
-        private void OnDisable()
-        {
-            GlobalTimer.OnTick -= ControlExplosion;
-        }
-
-        private void Start()
-        {
-            objPrefab.AddComponent<ExplosionObj>();
-        }
-        
-        private void ControlExplosion(float deltaTimeMS)
-        {
-            _msBuffer += deltaTimeMS;
-            while (_msBuffer > MS_PER_TICK)
-            {
-                _msBuffer -= MS_PER_TICK;
-                float v1 = (float)_random.NextDouble();
-                float v2 = _chanceFormula(GameStats.MainGameStatsHolder.DataUpgrades.ScreenExplosionLvl);
-                if (v1 > v2)
-                {
-                    Explode();
-                }
-            }
-        }
-
-        private void Explode()
-        {
-            Instantiate(objPrefab, gameObject.transform, false);
         }
     }
 }
