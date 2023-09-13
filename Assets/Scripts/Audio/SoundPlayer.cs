573c3b0996d91ea38275d059c2ee70874db42c94
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2023-09-13 17:06:22 +0500

    Sounds added, serialization encryption fixed

--- Assets/Scripts/Audio/SoundPlayer.cs
+++ Assets/Scripts/Audio/SoundPlayer.cs
@@ -1,6 +1,7 @@
 using System;
 using GameStatsNS;
 using MonoBehaviours;
+using MonoBehaviours.UpgradesNS;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -13,12 +14,14 @@ namespace Audio
         [SerializeField] private ClipHolder shotClip;
         [SerializeField] private ClipHolder chargedShotClip;
         [SerializeField] private ClipHolder destroyClip;
+        [SerializeField] private ClipHolder upgradeClip;
         private void OnEnable()
         {
             Enemy.OnAnyEnemyDestroy += PlayDestroyClip;
             Player.OnShot += PlayShotSound;
             Enemy.OnAnyEnemyDamageTaken += PlayImpactSound;
             Player.OnChargedShot += PlayChargedShotSound;
+            UpgradesButtonActions.OnUpgrade += PlayUpgradeClip;
         }
 
         private void OnDisable()
@@ -27,6 +30,7 @@ namespace Audio
             Player.OnShot -= PlayShotSound;
             Enemy.OnAnyEnemyDamageTaken -= PlayImpactSound;
             Player.OnChargedShot -= PlayChargedShotSound;
+            UpgradesButtonActions.OnUpgrade -= PlayUpgradeClip;
         }
         private void PlayImpactSound(Enemy _)
         {
@@ -46,6 +50,11 @@ namespace Audio
         private void PlayDestroyClip(Enemy _)
         {
             PlayIfNotOnCooldown(destroyClip);
+        }
+
+        private void PlayUpgradeClip(UpgradeEventArgs _)
+        {
+            PlayIfNotOnCooldown(upgradeClip);
         }
 
         private void PlayIfNotOnCooldown(ClipHolder clipHolder)
