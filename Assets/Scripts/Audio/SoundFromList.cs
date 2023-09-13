2967134bf4aa47942a07a3c1dfdb4a566455815f
Melkii-Mel <130223452+Melkii-Mel@users.noreply.github.com>
2023-09-13 19:00:27 +0500

    Button Click sounds added

---
+++ Assets/Scripts/Audio/SoundFromList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+namespace Audio
+{
+    /// <summary>
+    /// Used to play a random sound from a list
+    /// </summary>
+    public class SoundFromList : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioClip[] clips;
+
+        public SoundFromList(AudioClip[] clips)
+        {
+            this.clips = clips;
+        }
+
+        public void PlayRandom(GameObject emptyObject)
+        {
+            AudioSource player = Instantiate(emptyObject).AddComponent<AudioSource>();
+            player.PlayOneShot(clips[Random.Range(0, clips.Length)]);
+        }
+    }
+}
